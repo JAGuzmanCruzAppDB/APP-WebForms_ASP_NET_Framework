fe5fadd1f408875de88daffeeed8464f1afbcb24
JAGuzmanCruzAppDB <ingsistemasjosealfonso@gmail.com>
2023-01-26 22:22:58 -0600

    create y delete en procedimiento almacenado sql server

--- 02-00-CRUD-PROCEDURE/01-00-select_colum_in_GridView.aspx.cs
+++ 02-00-CRUD-PROCEDURE/01-00-select_colum_in_GridView.aspx.cs
@@ -16,17 +16,22 @@ namespace _02_00_CRUD_PROCEDURE
         {
             if (!IsPostBack)
             {
-                using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["connDB"].ConnectionString))
-                {
-                    SqlCommand cmd = new SqlCommand();
-                    cmd.CommandType = CommandType.StoredProcedure;
-                    cmd.CommandText = "SPSalumnos";
-                    cmd.Connection = conn;
-                    conn.Open();
-                    gvdAlumnos.DataSource = cmd.ExecuteReader();
-                    gvdAlumnos.DataBind();
-                }
-                /*tabla que se uso y la base de datos esta en el archivo (web.config) en  connectionStrings
+                CargaDatosAlumno();
+            }
+        }
+        public void CargaDatosAlumno()
+        {
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["connDB"].ConnectionString))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "SPSalumnos";
+                cmd.Connection = conn;
+                conn.Open();
+                gvdAlumnos.DataSource = cmd.ExecuteReader();
+                gvdAlumnos.DataBind();
+            }
+            /*tabla que se uso y la base de datos esta en el archivo (web.config) en  connectionStrings
                  create table alumno(
 	                id int identity(1,1),
 	                nombre varchar(50),
@@ -36,22 +41,89 @@ namespace _02_00_CRUD_PROCEDURE
 	                primary key(id) 
                  );
                  */
-                /* procedimiento almacenado ocupado en la consulta anterior
-                 create procedure SPSalumnos
-                as 
-                begin
-	            select  id 'Clave alumno'
-			            ,nombre 'Nombre'
-			            ,ap_paterno 'Apellido paterno'
-			            ,ap_materno 'Apellido materno'
-			            ,email 'Correo'
-		                from alumno
-                end
+            /* procedimiento almacenado ocupado en la consulta anterior
+             create procedure SPSalumnos
+            as 
+            begin
+            select  id 'Clave alumno'
+                    ,nombre 'Nombre'
+                    ,ap_paterno 'Apellido paterno'
+                    ,ap_materno 'Apellido materno'
+                    ,email 'Correo'
+                    from alumno
+            end
 
-                exec[SPSalumnos]
-                 */
-
+            exec[SPSalumnos]
+             */
+        }
+        public void GuardaAlumno()
+        {
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["connDB"].ConnectionString))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "SPIalumnos";
+                cmd.Parameters.Add("@nombre", SqlDbType.VarChar).Value = txtNombre.Text.Trim();
+                cmd.Parameters.Add("@ap_paterno", SqlDbType.VarChar).Value = txtAp_Paterno.Text.Trim();
+                cmd.Parameters.Add("@ap_materno", SqlDbType.VarChar).Value = txtAp_materno.Text.Trim();
+                cmd.Parameters.Add("@email", SqlDbType.VarChar).Value = txtEmail.Text.Trim();
+                cmd.Connection = conn;
+                conn.Open();
+                cmd.ExecuteNonQuery();
             }
+            /*procedimiento almacenado ocupado en la consulta anterior
+                create proc SPIalumnos
+	                @nombre varchar(50),
+	                @ap_paterno varchar(50),
+	                @ap_materno varchar(50),
+	                @email varchar(50)
+                as
+                begin
+	                insert into alumno(nombre,ap_paterno,ap_materno,email)
+					            values(@nombre,@ap_paterno,@ap_materno,@email);
+                end
+                 */
+        }
+        public void EliminarAlumno(string id)
+        {
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["connDB"].ConnectionString))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "SPDalumnos";
+                cmd.Parameters.Add("@id", SqlDbType.Int).Value = Int64.Parse(id);
+                cmd.Connection = conn;
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            /*procedimiento almacenado ocupado para eliminar registros
+             create proc SPDalumnos
+	                @id int
+             as begin
+	                delete from alumno where id=@id
+             end
+             */
+        }
+
+        protected void btnNuevo_Click(object sender, EventArgs e)
+        {
+            pnlDatoAlumno.Visible = false;
+            pnlAltaAlumno.Visible = true;
+        }
+
+        protected void btnGuardar_Click(object sender, EventArgs e)
+        {
+            pnlAltaAlumno.Visible = false;
+            pnlDatoAlumno.Visible = true;
+            GuardaAlumno();
+            CargaDatosAlumno();
+        }
+
+        protected void gvdAlumnos_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            GridViewRow row = (GridViewRow)gvdAlumnos.Rows[e.RowIndex];
+            EliminarAlumno(gvdAlumnos.DataKeys[e.RowIndex].Value.ToString());
+            CargaDatosAlumno();
         }
     }
 }
--- 02-00-CRUD-PROCEDURE/01-00-select_colum_in_GridView.aspx.designer.cs
+++ 02-00-CRUD-PROCEDURE/01-00-select_colum_in_GridView.aspx.designer.cs
@@ -24,6 +24,15 @@ namespace _02_00_CRUD_PROCEDURE
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// Control pnlDatoAlumno.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel pnlDatoAlumno;
+
+        /// <summary>
         /// Control gvdAlumnos.
         /// </summary>
         /// <remarks>
@@ -31,5 +40,104 @@ namespace _02_00_CRUD_PROCEDURE
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView gvdAlumnos;
+
+        /// <summary>
+        /// Control btnNuevo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnNuevo;
+
+        /// <summary>
+        /// Control pnlAltaAlumno.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel pnlAltaAlumno;
+
+        /// <summary>
+        /// Control lblNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblNombre;
+
+        /// <summary>
+        /// Control txtNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombre;
+
+        /// <summary>
+        /// Control lblAp_Paterno.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblAp_Paterno;
+
+        /// <summary>
+        /// Control txtAp_Paterno.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtAp_Paterno;
+
+        /// <summary>
+        /// Control lblAp_materno.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblAp_materno;
+
+        /// <summary>
+        /// Control txtAp_materno.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtAp_materno;
+
+        /// <summary>
+        /// Control lblEmail.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblEmail;
+
+        /// <summary>
+        /// Control txtEmail.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEmail;
+
+        /// <summary>
+        /// Control btnGuardar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnGuardar;
     }
 }
