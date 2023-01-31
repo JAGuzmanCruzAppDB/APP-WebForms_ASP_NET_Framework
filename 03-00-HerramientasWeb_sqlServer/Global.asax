3542d6c76fd165118d430278777fd1f4aa1b122b
JAGuzmanCruzAppDB <ingsistemasjosealfonso@gmail.com>
2023-01-31 10:02:57 -0600

    DropDownList

---
+++ 03-00-HerramientasWeb_sqlServer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_03_00_HerramientasWeb_sqlServer.Global" Language="C#" %>
---
+++ 03-00-HerramientasWeb_sqlServer/Site.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _03_00_HerramientasWeb_sqlServer
+{
+    public partial class SiteMaster : MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
